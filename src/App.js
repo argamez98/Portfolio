511d0a4f7193a287f891a19537adb487a4b29c38
argamez98 <argamez98@gmail.com>
2022-01-14 21:20:08 -0600

    added tailwind

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div className="App ">
+    <div className="App">
       <header className="App-header">
         <Navbar />
       </header>
