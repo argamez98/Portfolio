3532bc2c0713c579adae6093ca1ead0c2bb7b162
argamez98 <argamez98@gmail.com>
2022-01-14 21:07:12 -0600

    added navbar
    added navbar using react

---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Navbar (){
+    return (
+        <header>
+            <div className="navbar">
+                <a href="about">About Me</a>
+                <a>Projects</a>
+                <a href="https://drive.google.com/file/d/1PcYuX_8hKs8u1Hzt0N5PQixSFJU7nYQ5/view?usp=sharing">Resume</a>
+                <a>Contact</a>
+            </div>
+        </header>
+    );
+}
