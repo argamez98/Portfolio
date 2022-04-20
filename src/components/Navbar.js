bc60baa9c235d206d801885fd1e0ffd393345516
argamez98 <39465794+argamez98@users.noreply.github.com>
2022-04-19 22:06:57 -0500

    Update Navbar.js
    moved the navbar elements into a <navbar> element

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,15 @@
 import React from "react";
 
-export default function Navbar (){
+export default function Navbar() {
     return (
         <header>
-            <div className="navbar md:sticky top-0 z-10">
-                <a href="">About Me</a>
-                <a>Projects</a>
-                <a href="https://drive.google.com/file/d/1PcYuX_8hKs8u1Hzt0N5PQixSFJU7nYQ5/view?usp=sharing">Resume</a>
-                <a>Contact</a>
+            <div className="">
+                <nav className="navbar ">
+                    <a href="">About Me</a>
+                    <a>Projects</a>
+                    <a href="https://drive.google.com/file/d/1PcYuX_8hKs8u1Hzt0N5PQixSFJU7nYQ5/view?usp=sharing">Resume</a>
+                    <a>Contact</a>
+                </nav>
             </div>
         </header>
     );
