a1e297636148a439ea4ae4baa99795b7e4a5ba32
Ariel Gamez <39465794+argamez98@users.noreply.github.com>
2022-10-20 17:50:29 -0500

    fixing nameheader (unfinished)
    want to center my name in the middle of the page but can't seem to fix it. might be the margins? not sure. will look back later.

--- src/components/Nameheader.js
+++ src/components/Nameheader.js
@@ -2,8 +2,8 @@ import React from "react";
 
 export default function Nameheader() {
     return (
-        <section id="name">
-            <a className="title-font font-medium">Ariel Gamez</a>
-        </section>
+        <div class="name">
+            <a className="">Ariel Gamez</a>
+        </div>
     );
 }
