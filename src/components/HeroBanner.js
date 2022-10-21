55d6ca06bbee30af84f3d84c2291aca3ec84c986
Ariel Gamez <39465794+argamez98@users.noreply.github.com>
2022-10-20 23:19:04 -0500

    herobanner
    fixed some things and some css on the herobanner

--- src/components/HeroBanner.js
+++ src/components/HeroBanner.js
@@ -2,15 +2,14 @@ import React from 'react';
 
 export default function HeroBanner() {
     return (
-        <>
-            <div className='HeroBanner'>
-                <p>
-                    Hello there, I'm a 2020 Computer Science graduate from UTRGV,
-                    and I'm excited about looking for my first professional development role.
-                </p>
-            </div><div class="btn from-left">
-                <p class="btnText">About Me</p>
-            </div>
-        </>
+        <div className="herobanner">
+            <p>
+                I'm a 2020 computer science graduate from
+                <a href="https://www.utrgv.edu/" target="_blank"> UTRGV </a>
+            </p>
+            <p>
+                I was born and raised in a city called Mission in the RGV in south Texas
+            </p>
+        </div>
     );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,14 +2,14 @@ import React from "react";
 
 export default function Navbar() {
     return (
-            <div className="navbar">
-                <a className="logo"> My Logo </a>
-                <div class="navbar-right">
-                    <a href="">About Me</a>
-                    <a>Projects</a>
-                    <a href="https://drive.google.com/file/d/1PcYuX_8hKs8u1Hzt0N5PQixSFJU7nYQ5/view?usp=sharing">Resume</a>
-                    <a>Contact</a>
-                </div>
+        <div className="navbar">
+            <a className="logo"> My Logo </a>
+            <div class="navbar-right">
+                <a href="aboutme">About Me</a>
+                <a href="projects">Projects</a>
+                <a href="https://drive.google.com/file/d/1PcYuX_8hKs8u1Hzt0N5PQixSFJU7nYQ5/view?usp=sharing" target="_blank">Resume</a>
+                <a>Contact</a>
             </div>
+        </div>
     );
 }
