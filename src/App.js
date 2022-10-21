ce59d54f3ee945229d4a0532783f208788b4207c
Ariel Gamez <39465794+argamez98@users.noreply.github.com>
2022-10-21 15:39:21 -0500

    finished herobanner
    Added some sample text onto to the hero banner to get a visual of how the front page would look like.

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import Navbar from "./components/Navbar";
 import Nameheader from "./components/Nameheader";
 import About from "./components/About";
 import HeroBanner from './components/HeroBanner';
-import IconBar from './components/IconBar';
 
 function App() {
   return (
--- src/components/HeroBanner.js
+++ src/components/HeroBanner.js
@@ -4,11 +4,17 @@ export default function HeroBanner() {
     return (
         <div className="herobanner">
             <p>
-                I'm a 2020 computer science graduate from
-                <a href="https://www.utrgv.edu/" target="_blank"> UTRGV </a>
+                I'm a 2020 computer science graduate from 
+                <a href="https://www.utrgv.edu/" target="_blank">UTRGV</a>
             </p>
             <p>
-                I was born and raised in a city called Mission in the RGV in south Texas
+                I am from Mission, Texas located in the Rio Grande Valley
+            </p>
+            <p>
+                I made this website to be able to showcase my skills in web development and projects I'm working on
+            </p>
+            <p>
+                One of my side goals is to learn a skilled trade like carpentry or electrician
             </p>
         </div>
     );
