354d67737a10c1a24c4ed3461b3f9f3c607fcf78
Ariel Gamez <39465794+argamez98@users.noreply.github.com>
2023-02-21 22:11:58 -0600

    Update BigNameTitle.js
    changed padding on name to have it centered

--- src/components/BigNameTitle.js
+++ src/components/BigNameTitle.js
@@ -3,7 +3,7 @@ import React from "react";
 
 export default function BigNameTitle(){
     return (
-        <div class="flex-wrap text-center text-lg">
+        <div class="flex-wrap text-center text-lg pr-48">
             <p>Ariel Gamez</p>
         </div>
     );
