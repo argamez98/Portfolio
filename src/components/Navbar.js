629136b73f25a75abe8981b99c3a1baaeda808db
Ariel Gamez <39465794+argamez98@users.noreply.github.com>
2023-02-21 22:14:29 -0600

    Update Navbar.js
    changed resume link

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,7 +8,7 @@ export default function Navbar() {
             <div class="float-right">
                 <Link to="/aboutme">About Me</Link>
                 <Link to="/projects">Projects</Link>
-                <a href="https://drive.google.com/file/d/1PcYuX_8hKs8u1Hzt0N5PQixSFJU7nYQ5/view?usp=sharing" target="_blank">Resume</a>
+                <a href="https://drive.google.com/file/d/1k44zfKJuUgoXDOtDerreqj8j51wiP0MQ/view?usp=share_link" target="_blank">Resume</a>
                 <a>Contact</a>
             </div>
         </div>
