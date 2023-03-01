44f6bdd1adb26080298efa6691ef7b60392355b9
Ariel Gamez <39465794+argamez98@users.noreply.github.com>
2023-02-28 19:22:41 -0600

    Update BigNameTitle.js
    adding a description under my name

--- src/components/BigNameTitle.js
+++ src/components/BigNameTitle.js
@@ -5,6 +5,12 @@ export default function BigNameTitle(){
     return (
         <div class="flex-wrap text-center text-lg pr-48">
             <p>Ariel Gamez</p>
+
+            <body>
+                <p class="flex-wrap text-white pl-48 flex-grid sidebar">
+                    This is my personal website that I made to be able to showcase my HTML skills and to showcase my projects
+                </p>
+            </body>
         </div>
     );
 }
