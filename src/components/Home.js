19f762a38b236b1ee3f7e22f5ab12b72a08087ef
Ariel Gamez <39465794+argamez98@users.noreply.github.com>
2023-05-23 15:35:06 -0500

    removed  big name title to reduce redundancy

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,5 +3,11 @@ import BigNameTitle from "./BigNameTitle";
 
 export default function Home() {
     return (
-        <BigNameTitle/>);
+        <div className="titleblock justify-center text-center text-xxl bg-gradient-to-r from-gray-500 to-blue-700 bg-clip-text text-transparent">
+            <span>Ariel Gamez</span>
+            <div className='seperator'></div>
+            <span className="align-center bg-gradient-to-r from-gray-500 to-blue-700 bg-clip-text text-transparent">
+                Software Developer
+            </span>
+        </div>);
 }
